ff8c8e007682abbae7af0fa28dc7004dd412b19a
wkkuna <wildmarten00@gmail.com>
2023-06-07 09:25:15 +0200

    [AI] Refactor P2:EX2

--- AI/p2/ex2.py
+++ AI/p2/ex2.py
@@ -1,19 +1,17 @@
 import copy
 import heapq
 
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 direction_to_str = {(-1, 0): "U", (1, 0): "D", (0, -1): "L", (0, 1): "R"}
+directions = direction_to_str.keys()
 
-height = 0
-width = 0
+height, width = 0, 0
 board = []
-
 goals = set()
 visited = set()
 
 
-def is_not_wall(y, x):
-    return board[y][x] != "W"
+def is_wall(y, x):
+    return board[y][x] == "W"
 
 
 def is_terminal(state):
@@ -37,21 +35,44 @@ def calculate_distances():
         dist, pos = queue.pop(0)
         distances[pos[0]][pos[1]] = dist
 
-        for direction in directions:
-            new_pos = move(pos, direction)
+        for dxy in directions:
+            new_pos = move(pos, dxy)
 
-            if is_not_wall(new_pos[0], new_pos[1]) and new_pos not in visited:
+            if not is_wall(*new_pos) and new_pos not in visited:
                 queue.append((dist + 1, new_pos))
                 visited.add(new_pos)
 
     return distances
 
-
+# We want as little moves as possible
 def heuristic(start_state, distances):
     return sum([distances[crate[0]][crate[1]] for crate in start_state[0]])
 
 
-def search(start_state):
+def generate_states(state):
+    old_pos = state[1]
+    new_states = []
+
+    for dxy in directions:
+        crates = copy.deepcopy(state[0])
+        new_pos = move(old_pos, dxy)
+        one_ahead = move(new_pos, dxy)
+
+        if not is_wall(*new_pos):
+            if new_pos not in crates:
+                new_states.append(
+                    [crates, new_pos, state[2] + direction_to_str[dxy]])
+            # Some dead state detection
+            elif one_ahead not in crates and not is_wall(*one_ahead):
+                crates.remove(new_pos)
+                crates.add((new_pos[0] + dxy[0], new_pos[1] + dxy[1]))
+                new_states.append(
+                    [crates, new_pos, state[2] + direction_to_str[dxy]])
+
+    return new_states
+
+
+def run(start_state):
     if is_terminal(start_state):
         return start_state
 
@@ -71,67 +92,35 @@ def search(start_state):
         new_states = generate_states(state)
 
         for new_state in new_states:
-            value = visited.get((frozenset(new_state[0]), new_state[1]))
             cost = heuristic(new_state, distances) + len(new_state[2])
+            value = visited.get((frozenset(new_state[0]), new_state[1]))
 
             if value is None or value > cost:
                 heapq.heappush(queue, (cost, new_state))
                 visited[(frozenset(new_state[0]), new_state[1])] = cost
 
 
-def generate_states(state):
-    old_pos = state[1]
-    new_states = []
-
-    for direction in directions:
-        crates = copy.deepcopy(state[0])
-        new_pos = move(old_pos, direction)
-        one_ahead = move(new_pos, direction)
-
-        if is_not_wall(new_pos[0], new_pos[1]):
-            if new_pos not in crates:
-                new_states.append(
-                    [crates, new_pos, state[2] + direction_to_str[direction]])
-            elif one_ahead not in crates and is_not_wall(one_ahead[0], one_ahead[1]):
-                crates.remove(new_pos)
-                crates.add((new_pos[0] + direction[0],
-                           new_pos[1] + direction[1]))
-                new_states.append(
-                    [crates, new_pos, state[2] + direction_to_str[direction]])
-
-    return new_states
-
-
-if __name__ == "__main__":
-    with open("zad_input.txt", "r") as f:
-        for line in f:
-            height += 1
-            board.append(line.strip())
-
-    width = len(board[0])
-    start_state = [set(), (0, 0), '']
+# Parse the board symbols to game state
+# Returns the initial state of the game
+def init_game():
+    global board, height, width, player, crates
+    height, width = len(board), len(board[0])
     player = (0, 0)
     crates = set()
 
     for y in range(height):
         for x in range(width):
-            char = board[y][x]
-
-            if char == 'K':
+            c = board[y][x]
+            if c in ['G', '*', '+']:
+                goals.add((y, x))
+            if c in ['K', '+']:
                 player = (y, x)
-            if char == 'B':
+            if c in ['B', '*']:
                 crates.add((y, x))
-            if char == 'G':
-                goals.add((y, x))
-            if char == '*':
-                crates.add((y, x))
-                goals.add((y, x))
-            if char == '+':
-                player = (y, x)
-                goals.add((y, x))
+    return [crates, player, '']
 
-    start_state = [crates, player, '']
-    end_state = search(start_state)
 
-    with open("zad_output.txt", 'w') as f:
-        f.write(end_state[2])
+if __name__ == "__main__":
+    with open("zad_input.txt", "r") as f, open("zad_output.txt", 'w') as out:
+        board = list(map(lambda x: x.strip(), f.readlines()))
+        out.write(run(init_game())[2])
